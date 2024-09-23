d40c89b51fa9612793080b9364c9c7afd5551a45
farzaadshojaei <farzaadshojaei@gmail.com>
2024-09-23 21:44:30 +0330

    Completing LinkedList

---
+++ src/LinkedList.java
@@ -0,0 +1,49 @@
+public class LinkedList {
+    class Node{
+        int value;
+        Node next;
+
+        Node(int value){
+            this.value=value;
+        }
+
+    }
+    private Node head;
+    private Node tail;
+    private int length;
+
+
+    public LinkedList(int value){
+        Node newNode=new Node(value);
+        head=newNode;
+        length=1;
+    }
+    public void printList(){
+        Node temp= head;
+        while(temp != null){
+            System.out.println(temp.value);
+            temp=temp.next;
+        }
+    }
+
+    public void getLength(){
+        System.out.println("Length"+ length);
+    }
+    public void getTail(){
+        System.out.println("Tail"+ tail.value);
+    }
+    public void getHead(){
+        System.out.println("Head" + head.value);
+    }
+
+    public void append(int value){}
+    public void prepend(int value){}
+
+    public boolean insert(int index,int value){
+        return false;
+    }
+
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+
+     //   HashMap<String,Integer> map1=new HashMap<>();
+     //   HashMap<String,Integer> map2=new HashMap<>();
+     //   map1.put("value",11);
+     //   map2=map1;
+
+
+     //   map1.put("value",22);
+     //   System.out.println(map1);
+    //    System.out.println(map2);
+     //   Cookie cookieOne=new Cookie("green");
+     //   Cookie cookieTwo=new Cookie("Blue");
+
+      //  cookieOne.setColor("Yellow");
+
+     //   System.out.println(cookieOne.getColor());
+     //   System.out.println(cookieTwo.getColor());
+
+
+       LinkedList mylinkedlist=new LinkedList(4);
+
+       mylinkedlist.getHead();
+       mylinkedlist.getLength();
+       mylinkedlist.getTail();
+
+       mylinkedlist.printList();
+
+
+    }
+}
